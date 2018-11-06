a125c29d3d14fd388cff7c547b83af1a012b3901
Ravi Anand <ravi.anand@nbt.ag>
2018-11-06 12:25:58 +0100

    Test commit on master branch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome in Vue Production App... now add project to git."/>
   </div>
 </template>
 
