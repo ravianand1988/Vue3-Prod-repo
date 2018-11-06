8f4574211fab2f3205988278765cfe3a6f6ea8b9
Ravi Anand <ravi.anand@nbt.ag>
2018-11-06 15:59:00 +0100

    Add Vuetify Icon.
    - Use Vuetify in project
    - Add material Design icons
    - Add webfontloader

--- src/main.js
+++ src/main.js
@@ -1,12 +1,36 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Vuetify from 'vuetify'
+import WebFontLoader from 'webfontloader'
 
-Vue.config.productionTip = false;
+//import assets
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.config.productionTip = false
+
+Vue.use(Vuetify)
+
+export default {
+  mounted () {
+    WebFontLoader.load({
+      google: {
+        families: ['Roboto:100,300,400,500,700,900']
+      },
+      active: this.setFontLoaded
+    })
+  },
+  methods: {
+    setFontLoaded () {
+      this.$emit('font-loaded')
+    }
+  }
+}
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
